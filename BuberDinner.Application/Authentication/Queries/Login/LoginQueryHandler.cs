17a5addcc29a9fc38f7728aa72d44e70b11f4975
izaquiel Nascimento <izaquiel.nascimento@premiersoft.net>
2022-11-25 22:05:24 -0300

    add: CQRS and MediatR

---
+++ BuberDinner.Application/Authentication/Queries/Login/LoginQueryHandler.cs
@@ -0,0 +1,39 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Domain.Entities;
+using BuberDinner.Domain.Common.Errors;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Authentication.Queries.Login;
+
+public class LoginQueryHandler :
+    IRequestHandler<LoginQuery, ErrorOr<AuthenticationResult>>
+{
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
+
+    public LoginQueryHandler(IUserRepository userRepository, IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _userRepository = userRepository;
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
+    public async Task<ErrorOr<AuthenticationResult>> Handle(LoginQuery query, CancellationToken cancellationToken)
+    {
+        if (_userRepository.GetUserByEmail(query.Email) is not User user)
+        {
+            return Errors.Authentication.InvalidCredentials;
+        }
+
+        if (user.Password != query.Password)
+        {
+            return Errors.Authentication.InvalidCredentials;
+        }
+
+        var token = _jwtTokenGenerator.GenerateToken(user);
+
+        return new AuthenticationResult(user, token);
+    }
+}
